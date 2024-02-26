2fa94ec8c5817324a90743e67a56d36d3b58780d
Ismail ZAHIR <ismailza407@gmail.com>
2024-02-26 22:57:20 +0100

    new update

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById('weatherForm');
-  form.addEventListener('submit', function(e) {
+  form.addEventListener('submit', function (e) {
     e.preventDefault();
     const city = document.getElementById('city').value;
     fetch('/weather', {
@@ -11,57 +11,77 @@ document.addEventListener("DOMContentLoaded", function() {
       },
       body: `city=${encodeURIComponent(city)}`
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('City not found');
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.cod === "404") {
-        displayErrorMessage(data.message);
-      } else {
-        displayWeather(data);
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      displayErrorMessage(error.message);
-    });
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('City not found');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.cod === "404") {
+          displayErrorMessage(data.message);
+        } else {
+          displayWeather(data);
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        displayErrorMessage(error.message);
+      });
   });
 });
 
+const weatherIcons = {
+  'Clear': 'clear.png',
+  'Clouds': 'clouds.png',
+  'Rain': 'rain.png',
+  'Snow': 'snow.png',
+  'Thunderstorm': 'thunderstorm.png',
+  'Drizzle': 'drizzle.png',
+  'Mist': 'mist.png',
+};
 
 const displayWeather = (data) => {
-  const resultDiv = document.getElementById('weatherResult');
+  let resultDiv = document.getElementById('weatherResult');
   if (!resultDiv) {
-    const newDiv = document.createElement('div');
-    newDiv.id = 'weatherResult';
-    newDiv.innerHTML = `
-      <h3>Weather in ${data.name}</h3>
-      <p>Temperature: ${data.main.temp - 273.15}°C</p>
-      <p>Weather: ${data.weather[0].description}</p>
-    `;
-    document.body.appendChild(newDiv);
-  } else {
-    resultDiv.innerHTML = `
-      <h3>Weather in ${data.name}</h3>
-      <p>Temperature: ${data.main.temp - 273.15}°C</p>
-      <p>Weather: ${data.weather[0].description}</p>
-    `;
+    resultDiv = document.createElement('div');
+    resultDiv.id = 'weatherResult';
+    document.body.appendChild(resultDiv);
   }
+  resultDiv.classList.add('container', 'mt-2', 'p-3', 'border', 'rounded');
+  let temperatureCelsius = (data.main.temp - 273.15).toFixed(2);
+  let weatherIcon = weatherIcons[data.weather[0].main] || 'clear.png';
+
+  resultDiv.innerHTML = `
+    <div class="row">
+      <div class="col-md-8">
+        <h3>Weather in ${data.name}</h3>
+        <p class="text-capitalize fs-5 text-muted text-secondary">${data.weather[0].description}</p>
+        <div class="details">
+          <p>Temperature: ${temperatureCelsius}°C</p>
+          <p>Humidity: ${data.main.humidity}%</p>
+          <p>Pressure: ${data.main.pressure}hPa</p>
+          <p>Wind speed: ${data.wind.speed}m/s</p>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex align-items-center justify-content-center">
+        <div class="weather-icon">
+          <img src="/images/${weatherIcon}" alt="${weatherIcon}" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  `;
 };
+
 
 const displayErrorMessage = (message) => {
   const resultDiv = document.getElementById('weatherResult');
   if (!resultDiv) {
     const newDiv = document.createElement('div');
     newDiv.id = 'weatherResult';
-    newDiv.innerHTML = `<p>${message}</p>`;
     document.body.appendChild(newDiv);
-  } else {
-    resultDiv.innerHTML = `<p>${message}</p>`;
   }
+  resultDiv.innerHTML = `<p>${message}</p>`;
 };
 
 (() => {
